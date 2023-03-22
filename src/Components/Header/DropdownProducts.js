716d8ebc618fcc4e115da0b0bf823128a3a98620
Josh Hobson <joshuahobson1995@gmail.com>
2023-03-22 12:38:01 -0600

    ntw

--- src/Components/Header/DropdownProducts.js
+++ src/Components/Header/DropdownProducts.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './DropdownProducts.css'
 
+
 function DropdownProducts() {
   return (
     <div className='dropdown_products'>
         <div className='menu'>
             <li className='m_top'>New This Week</li>
-            <li> Specials/Sales</li>
+            <li> Sales</li>
             <li>Brands we Carry</li>
             <li>Southwest Plant Catalog</li> 
         </div>
--- src/Components/New This Week/NewThisWeek.js
+++ src/Components/New This Week/NewThisWeek.js
@@ -3,6 +3,7 @@ import './NewThisWeek.css'
 import NTWgallery from './NTW_Gallery/NTWgallery'
 import img1 from '../../Assets/cf1.jpg'
 import img2 from '../../Assets/cf2.jpg'
+import SeminarIcon from '../../SVGs/SeminarIcon'
 
 function NewThisWeek() {
   return (
@@ -16,26 +17,37 @@ function NewThisWeek() {
         <div className='ntw_gallery'>
             <NTWgallery />
         </div>
-        <div className='ntw_body'>
+        <div className='ntw_body_main'>
             <div className='ntw_body_top'>
-            <h1 className='march'>March is Still </h1><h1 className='cold'>Cold</h1>
+                <div className='bt_header'>
+                         <h1 className='march'>March is Still </h1><h1 className='cold'>Cold</h1>
+                </div>
             <p>
-            Welcome Statement:  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Welcome Statement:  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             </p>
                 <div className='ntw_body_imgs'>
-                    <img alt='' src={img1}/>
-                    <img alt='' src={img2}/>
+                    <img alt='' src={img1} className='ntw_img_1'/>
+                    <img alt='' src={img2} className='ntw_img_2'/>
                 </div>
             </div>
 
         <div className='ntw_body_bottom'>
             <h1>At least there's indoors</h1>
+
+    <div className='bb_column'>
+
+    
+        <div className='seminar_icon'>
+            <span>Perfect Time for Seminars</span>
+            <SeminarIcon />
+        </div>
+
             <p>
-            Welcome Statement:  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Welcome Statement:  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             </p>
             
             
-
+        </div>
             </div>
     </div>
         <div className='ntw_conclusion'>
