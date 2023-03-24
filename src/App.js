61b70882ee432c9e359c11ab80a37d3a993c4040
Josh Hobson <joshuahobson1995@gmail.com>
2023-03-24 13:08:16 -0600

    gardening calendar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header'
 import Home from './Components/Home/Home';
 import NewThisWeek from './Components/New This Week/NewThisWeek';
 import FFC from './Components/FFC/FFC';
+import GardeningCalendar from './Components/GardeningCalendar/GardeningCalendar';
 import Footer from './Components/Footer/Footer';
 import './App.css';
 
@@ -13,7 +14,8 @@ function App() {
       <Routes>
        <Route path='/' element={<Home />}/>
        <Route path='/newthisweek' element={<NewThisWeek />}/>
-        <Route path='/ffc' element={<FFC />}/>
+       <Route path='/ffc' element={<FFC />}/>
+       <Route path='gardeningcalendar' element={<GardeningCalendar/>}/>
       </Routes>
       <Footer />
     </div>
