2b0373b599ed3b0c0533ff74da94576e5d588970
Josh Hobson <joshuahobson1995@gmail.com>
2023-03-27 17:31:46 -0600

    header yeah!

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 import balloons from '../../Assets/Cropped_balloons.jpg'
 import logo from '../../Assets/Logo.png'
@@ -9,7 +10,7 @@ import GiftCardIcon from '../../SVGs/GiftCardIcon'
 import BlogIcon from '../../SVGs/BlogIcon'
 import CommunityIcon from '../../SVGs/CommunityIcon'
 import FFCIcon from '../../SVGs/FFCIcon'
-import DropdownProducts from './DropdownProducts'
+
 
 
 
@@ -32,58 +33,110 @@ function Header() {
 
 
         <div className='photo_banner'>
-            <img className='balloons' src={balloons} alt='ballons'/>
+        <img className='balloons' src={balloons} alt='ballons'/>
+          <NavLink to='/'>
             <div className='logo_div'>
                 <img className='logo' src={logo} alt='logo'/>
             </div>
+          </NavLink>
         </div>
-
-
+{/* --------------------------------------------------------- */}
         <div className='navbar'>
   <div className='nav_item'>
-    <div className='products_icon'>
+    <div className='nav_products'>
+      <div className='products_icon'>
     <ProductsIcon />
-    <DropdownProducts />
- 
-      
+      <ul>
+        <NavLink to='/newthisweek'>
+        <li>New This Week</li>
+        </NavLink>
+        <li>Sales</li>
+        <li>Brands we Carry</li>
+        <li>Southwest Plants</li>
+      </ul>
     <span>Products</span>
 
-  
+      </div>
     </div>
   </div>
+{/* ----------------------------------------------------------- */}
   <div className='nav_item'>
+    <div className='nav_giftcard'>
     <div className='gift_card_icon'>
     <GiftCardIcon />
-   
-    </div>
+    <ul>
+        <li>Buy a Card </li>
+      </ul>
     <span>Gift Cards</span>
+      </div>
+    </div>
   </div>
+{/* ----------------------------------------------------------- */}
   <div className='nav_item'>
+    <div className='nav_blog'>
     <div className='blog_icon'>
       <BlogIcon />
-    </div>
+      <ul>
+        <li>Blogs</li>
+        <li>Galleries</li>
+        <NavLink to='/gardeningcalendar'>
+        <li>Gardening Calendar</li>
+        </NavLink>
+        <li>Fresh Ideas</li>
+      </ul>
     <span>Blog and Gallery</span>
+      </div>
+    </div>
   </div>
+  
+{/* ----------------------------------------------------------- */}
   <div className='nav_item'>
+    <div className='nav_community'>
     <div className='community_icon'>
       <CommunityIcon />
-    </div>
+      <ul>
+        <li>Partners</li>
+        <li>Radio Show</li>
+        <li>Seminars</li>
+        <li>Clubs around Abq</li>
+      </ul>
     <span>Community</span>
+      </div>
+    </div>
   </div>
+
+{/* ----------------------------------------------------------- */}
   <div className='nav_item'>
+    <div className='nav_ffc'>
     <div className='ffc_icon'>
       <FFCIcon />
-    </div>
+      <ul>
+        <NavLink to='/ffc'>
+        <li>Join the Frequent Flower Club </li>
+        </NavLink>
+      </ul>
+
     <span>Frequent Flower Club</span>
+      </div>
+    </div>
   </div>
+  {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
+    <div className='nav_contactus'>
     <div className='contactus'>
     <ContactUsIcon />
-    </div>
+    <ul>
+        <li>Location info</li>
+        <li>Contact Info</li>
+        <li>Why Work Here</li>
+        <li>Phone Number</li>
+      </ul>
     <span>Contact Us</span>
+      </div>
+    </div>
   </div>
 </div>
-
+{/* ----------------------------------------------------------- */}
 
 
 
