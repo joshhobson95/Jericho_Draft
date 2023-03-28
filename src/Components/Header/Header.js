21340b808ce72cb3a9dc016a6842163efa75d412
Josh Hobson <joshuahobson1995@gmail.com>
2023-03-28 15:12:46 -0600

    fixed nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,13 +17,49 @@ import FFCIcon from '../../SVGs/FFCIcon'
 
 function Header() {
 
+const [showProducts, setShowProducts] = useState(false);
+const [showGiftcard, setShowGiftcard] = useState(false);
+const [showBlog, setShowBlog] = useState(false);
+const [showCommunity, setShowCommunity] = useState(false);
+const [showFrequentFlowerClub, setShowFrequentFlowerClub] = useState(false);
+const [showContact, setShowContact] = useState(false);
+const [animateDrop, setAnimateDrop] = useState(0)
 
-// const [isOpen, setIsOpen] = useState(false)
+function toggleProducts(){
+  setShowProducts(!showProducts)
+}
+function toggleGiftcard(){
+  setShowGiftcard(!showGiftcard)
+}
+function toggleBlog(){
+  setShowBlog(!showBlog)
+}
+function toggleCommunity(){
+  setShowCommunity(!showCommunity)
+}
+function toggleFrequentFlowerClub(){
+  setShowFrequentFlowerClub(!showFrequentFlowerClub)
+}
+function toggleContact(){
+  setShowContact(!showContact)
+}
 
-// const handleToggle = () => {
-//   setIsOpen(!isOpen);
+function closeMenus(){
+  setShowProducts(false)
+  setShowGiftcard(false)
+  setShowBlog(false)
+  setShowCommunity(false)
+  setShowFrequentFlowerClub(false)
+  setShowContact(false)
+}
+
+
+// const menuStyles = {
+//   height: showProducts || showGiftcard|| showBlog|| showCommunity || showFrequentFlowerClub || showContact ? 'auto' : '0',
+//   transition: 'height 0.3s ease-in-out',
+//   overflow: 'hidden',
+//   Animation: ''
 // };
-
 
   
   return (
@@ -44,38 +80,48 @@ function Header() {
         <div className='navbar'>
   <div className='nav_item'>
     <div className='nav_products'>
-      <div className='products_icon'>
+      <div className='products_icon' onClick={toggleProducts} >
     <ProductsIcon />
-      <ul>
-        <NavLink to='/newthisweek'>
-        <li>New This Week</li>
-        </NavLink>
-        <li>Sales</li>
-        <li>Brands we Carry</li>
-        <li>Southwest Plants</li>
-      </ul>
     <span>Products</span>
-
       </div>
-    </div>
+
+      {showProducts && (
+        <ul>
+          <NavLink to='/newthisweek'>
+            <li>New This Week</li>
+          </NavLink>
+            <li>Sales</li>
+            <li>Brands we Carry</li>
+            <li>Southwest Plants</li>
+          </ul>
+        )}
+        </div>
   </div>
 {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
     <div className='nav_giftcard'>
-    <div className='gift_card_icon'>
+    <div className='gift_card_icon' onClick={toggleGiftcard}>
     <GiftCardIcon />
+    <span>Gift Cards</span>
+      </div>
+
+{showGiftcard && (
     <ul>
         <li>Buy a Card </li>
       </ul>
-    <span>Gift Cards</span>
-      </div>
+)}
+
     </div>
   </div>
 {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
     <div className='nav_blog'>
-    <div className='blog_icon'>
+    <div className='blog_icon' onClick={toggleBlog}>
       <BlogIcon />
+    <span>Blog and Gallery</span>
+      </div>
+
+{showBlog && (
       <ul>
         <li>Blogs</li>
         <li>Galleries</li>
@@ -86,55 +132,63 @@ function Header() {
         <li>Fresh Ideas</li>
         </NavLink>
       </ul>
-    <span>Blog and Gallery</span>
-      </div>
+)}
     </div>
   </div>
   
 {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
     <div className='nav_community'>
-    <div className='community_icon'>
+    <div className='community_icon' onClick={toggleCommunity}>
       <CommunityIcon />
+    <span>Community</span>
+      </div>
+
+{showCommunity && (
       <ul>
         <li>Partners</li>
         <li>Radio Show</li>
         <li>Seminars</li>
         <li>Clubs around Abq</li>
       </ul>
-    <span>Community</span>
-      </div>
+)}
     </div>
   </div>
 
 {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
     <div className='nav_ffc'>
-    <div className='ffc_icon'>
+    <div className='ffc_icon' onClick={toggleFrequentFlowerClub}>
       <FFCIcon />
+    <span>Frequent Flower Club</span>
+      </div>
+
+      {showFrequentFlowerClub && (
       <ul>
         <NavLink to='/ffc'>
         <li>Join the Frequent Flower Club </li>
         </NavLink>
       </ul>
+      )}
 
-    <span>Frequent Flower Club</span>
-      </div>
     </div>
   </div>
   {/* ----------------------------------------------------------- */}
   <div className='nav_item'>
     <div className='nav_contactus'>
-    <div className='contactus'>
+    <div className='contactus' onClick={toggleContact}>
     <ContactUsIcon />
+    <span>Contact Us</span>
+      </div>
+{showContact &&(
     <ul>
         <li>Location info</li>
         <li>Contact Info</li>
         <li>Why Work Here</li>
         <li>Phone Number</li>
       </ul>
-    <span>Contact Us</span>
-      </div>
+)}
+
     </div>
   </div>
 </div>
