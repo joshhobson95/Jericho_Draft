7e9721bf8510b05a85efa0569882f2d24979dd2f
Josh Hobson <joshuahobson1995@gmail.com>
2023-04-04 14:01:11 -0600

    radio css

--- src/Components/Radio/Radio.js
+++ src/Components/Radio/Radio.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Radio.css'
-import RadioClouds from '../../SVGs/RadioClouds'
+import radiorick from '../../Assets/radiorick2.JPG'
 import RadioMic from '../../SVGs/RadioMic'
 
 function Radio() {
@@ -11,14 +11,18 @@ function Radio() {
             <p>
                 Garden Talk with Rick Hobson has been on air for over 15 years and counting. Tune in every other saturday to hear everything you need to know about Albuqueruque gardening, southwestern horticulture and all kinds of plants from zone 5-8.
             </p>
-            <div className='radio_mic'>
-                <RadioMic />
-            </div>
+    
         </div>
 
 
         <div className='radio_body_main'>
+
+            <div className='radio_body_top'>
+
             <h1>Garden Talk</h1>
+     <div className='radio_mic'>
+                <RadioMic />
+            </div>
             <h3>
                 TUNE IN TO "GARDEN TALK" 11:00 AM SATURDAYS
             </h3>
@@ -26,14 +30,18 @@ function Radio() {
                 KKOB | 96.3 FM | 770 AM 
             </h2>
 
-     <div className='radio_body_mid'>
-            <div className='radio_clouds'>
-                <RadioClouds />
-            </div>
 
+
+</div>
+     <div className='radio_body_mid'>
+
+<div className='rbm_section'>
+        <img alt='' src={radiorick} className='radio_rick'/>
+        <h2>Meet the Host</h2>
         <p>
         Rick Hobson is the owner of his own thriving business, Jericho Nursery, and host of the successful call-in gardening show, New Mexico Garden Talk on KKOB. Rick Hobson worked at Rowland’s for over 25 years and is considered an expert in the Albuquerque gardening community and around the state. Rick is on numerous boards, including the Japanese beetle taskforce; was acting president of New Mexico Association of Independent Nursery Industries and is currently assisting the New Mexico chapter of the Colorado Nursery and Greenhouse Association to create testing programs for nursery, landscape and greenhouse employees.
         </p>
+</div>
      </div>
 
 
