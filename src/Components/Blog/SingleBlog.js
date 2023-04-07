449d78937f838fd98934945ee3c4f542747772ec
Josh Hobson <joshuahobson1995@gmail.com>
2023-04-06 18:19:17 -0600

    mobile styling done

--- src/Components/Blog/SingleBlog.js
+++ src/Components/Blog/SingleBlog.js
@@ -11,7 +11,6 @@ function SingleBlog() {
 
       <div className='blog_welcome'>
           {BlogData.filter(item => item.id === 2).map((item) =>(
-            
             <div>
               <h1>Blog Post:</h1>
               <h2> Selected: {item.title}</h2>
@@ -20,113 +19,138 @@ function SingleBlog() {
             </div>
           ))}
       </div>
+      
 
-
-        {BlogData.filter(item => item.id === 2).map((item) => (
-    <div className='blog_body_main'> 
-        <div className='blog_body_top'>
+      {BlogData.filter(item => item.id === 3).map((item) => (
+<div key={item.id} className='blog_body_main'> 
+    <div className='blog_body_top'>
             <h1 className='blog_title'>{item.title}</h1>
-            <img alt=''  src={item.img_1_url} className='blog_img'/>
-            <p>{item.img_1_captions}</p>
-            <img alt='' src={item.img_2_url} className='blog_img'/>
-            <p>{item.img_2_captions}</p>
 
-            <p>{item.intro}</p>
+            {item.img_1_url && <img alt=''  src={item.img_1_url} className='blog_img'/>}
+            {item.img_1_captions && <p>{item.img_1_captions}</p>}
+            {item.img_2_url && <img alt='' src={item.img_2_url} className='blog_img'/>}
+            {item.img_2_captions && <p>{item.img_2_captions}</p>}
+
 
             <div className='blog_icon_1'>
-               <img alt='' src={item.icon}/> 
-             </div>   
-
-             <div className='blog_paragraph_top'>
-              <img alt='' src={item.body_img_1} className='body_img_1'/> 
-              <span className='span_yellow'>{item.span_yellow}</span>
-                <p>{item.body_1}</p>
+              {item.icon && <img alt='' src={item.icon}/> }
+            </div>   
+            {item.intro && <p>{item.intro}</p>}
 
 
-                <h6>{item.link}</h6>
-               <NavLink to={item.a_tag_1}>
-                <button>{item.button1_name}</button>
-               </NavLink>
-            
+            <div className='blog_paragraph_top'>
+              {item.body_img_1 && <img alt='' src={item.body_img_1} className='body_img_1'/>}
+              {item.span_yellow && <span className='span_yellow'>{item.span_yellow}</span>}
+              {item.body_1 && <p>{item.body_1}</p>}
+              {item.link && <h6>{item.link}</h6>}
+              {item.a_tag_1 && (
+                <NavLink to={item.a_tag_1}>
+                  <button>{item.button1_name}</button>
+                </NavLink>
+              )}
             </div>
-        </div>
+
+      </div>
 
 {/* 
       //////////////////////////////////////////////////////////////////////////////////////// */}
 
 
 
-        <div className='blog_body_mid'>
-            <h1>{item.title2}</h1>
-            <img alt=''  src={item.img_3_url} className='blog_img'/>
-            <p>{item.img_3_captions}</p>
-            <img alt='' src={item.img_4_url} className='blog_img'/>
-            <p>{item.img_4_captions}</p>
+<div className='blog_body_mid'>
+  <h1>{item.title2}</h1>
+  {item.img_3_url && <img alt='' src={item.img_3_url} className='blog_img'/>}
+  {item.img_3_captions && <p>{item.img_3_captions}</p>}
+  {item.img_4_url && <img alt='' src={item.img_4_url} className='blog_img'/>}
+  {item.img_4_captions && <p>{item.img_4_captions}</p>}
 
-            <div className='blog_icon_2'>
-               <img alt='' src={item.icon2}/> 
-             </div>   
+  <div className='blog_icon_2'>
+    {item.icon2 && <img alt='' src={item.icon2}/> }
+  </div>   
 
-             <div className='blog_paragraph_mid'>
-             <img alt='' src={item.body_img_2} className='body_img_2'/> 
-                <p>{item.body_2}</p>
-                <span className='span_green'>{item.span_green}</span>
+  <div className='blog_paragraph_mid'>
+    {item.body_img_2 && <img alt='' src={item.body_img_2} className='body_img_2'/>}
+    {item.body_2 && <p>{item.body_2}</p>}
+    {item.span_green && <span className='span_green'>{item.span_green}</span>}
 
-                <img alt=''  src={item.img_5_url} className='blog_img'/>
-                 <p>{item.img_5_captions}</p>
+    {item.img_5_url && <img alt='' src={item.img_5_url} className='blog_img'/>}
+    {item.img_5_captions && <p>{item.img_5_captions}</p>}
 
+    {item.body_img_3 && <img alt='' src={item.body_img_3} className='body_img_3'/>}
+    {item.body_3 && <p>{item.body_3}</p>}
 
-                 <img alt='' src={item.body_img_3} className='body_img_3'/> 
-                <p>{item.body_3}</p>
-
-                <h6>{item.link2}</h6>
+    <h6>{item.link2}</h6>
+    {item.a_tag_2 && (
                 <NavLink to={item.a_tag_2}>
                   <button>{item.button2_name}</button>
                 </NavLink>
-             </div>   
+              )}
+  </div>   
 
-<div className='extra_content'>
-            <img alt='' src={item.img_6_url} className='blog_img'/>
-            <p>{item.img_6_captions}</p>
-            <img alt='' src={item.img_7_url} className='blog_img'/>
-            <p>{item.img_7_captions}</p>
+  <div className='extra_content'>
+    {item.img_6_url && <img alt='' src={item.img_6_url} className='blog_img'/>}
+    {item.img_6_captions && <p>{item.img_6_captions}</p>}
+    {item.img_7_url && <img alt='' src={item.img_7_url} className='blog_img'/>}
+    {item.img_7_captions && <p>{item.img_7_captions}</p>}
+  </div>
 </div>
 
-        </div>   
-
 {/* //////////////////////////////////////////////////////////////////////////////////////// */}
+
 
 
 <p>Overflow Gallery Placeholder</p>
 
 
-            <div className='blog_body_bottom'>
+<div className='blog_body_bottom'>
+  {item.img_8_url && (
+    <div>
+      <img alt='' src={item.img_8_url} className='blog_img'/>
+      <p>{item.img_8_captions}</p>
+    </div>
+  )}
+  {item.img_9_url && (
+    <div>
+      <img alt='' src={item.img_9_url} className='blog_img'/>
+      <p>{item.img_9_captions}</p>
+    </div>
+  )}
+  <div className='blog_paragraph_bottom'>
+    {item.span_brown && (
+      <span className='span_brown'>{item.span_brown}</span>
+    )}
+    {item.body_img_4 && (
+      <img alt='' src={item.body_img_4} className='body_img_4'/> 
+    )}
+    {item.body_4 && (
+      <p>{item.body_4}</p>
+    )}
+    {item.icon3 && (
+      <h6>{item.icon3}</h6>
+    )}
+    {item.link3 && (
+      <p>{item.link3}</p>
+    )}
+  </div>   
+
+  {item.img_10_url && (
+    <>
+      <img alt='' src={item.img_10_url} className='blog_img'/>
+      <p>{item.img_10_captions}</p>
+    </>
+  )}
+
+  {item.conclusion && (
+    <p>{item.conclusion}</p>
+  )}
 
 
-            <img alt='' src={item.img_8_url} className='blog_img'/>
-            <p>{item.img_8_captions}</p>
-            <img alt='' src={item.img_9_url} className='blog_img'/>
-            <p>{item.img_9_captions}</p>
-
-            
-
-            
-            <div className='blog_paragraph_bottom'>
-            <span className='span_brown'>{item.span_brown}</span>
-            <img alt='' src={item.body_img_4} className='body_img_4'/> 
-                <p>{item.body_4}</p>
-                <h6>{item.icon3}</h6>
-                <p>{item.link3}</p>
-             </div>   
-
-
-            <img alt='' src={item.img_10_url} className='blog_img'/>
-            <p>{item.img_10_captions}</p>
-
-                <p>{item.conclusion}</p>
-                <img alt='' src={item.icon4}/>
-
-            </div>
+  {item.icon4 && (
+    <div className='blog_icon_3'>
+    <img alt='' src={item.icon4}/>
+    </div>
+  )}
+</div>
 
     </div>
 ))}
